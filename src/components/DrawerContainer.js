dcae803b7ae8b0c26edc450842cf4bf447314445
Jose Collas <joseherminiocollas@goatstonc.com>
2019-11-26 11:29:42 -0800

    refactor control and state properties

--- src/components/DrawerContainer.js
+++ src/components/DrawerContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './DrawerContainer.css'
 
@@ -6,16 +6,29 @@ const DrawerAlign = {
   LEFT: 'left',
   RIGHT: 'right',
 }
-
 export { DrawerAlign }
 
 const DrawerContainer = ({ children, yPos = 20, alignX = DrawerAlign.LEFT }) => {
-  const openX = 0
-  const closeX = -250
-  // isOpen isClosed TODO
   // swap the alignments for the button
   const buttonPostion = alignX === DrawerAlign.LEFT ? DrawerAlign.RIGHT : DrawerAlign.LEFT
-  const [xPos, setXPos] = useState(closeX)
+  // control hook: open closed state
+  const [isOpen, setIsOpen] = useState(true)
+  // component state
+  const buttonSymbols = { OPEN: 'Open', CLOSED: 'Close' }
+  const xPositions = { OPEN: 0, CLOSED: -250 }
+  const [xPos, setXPos] = useState(xPositions.OPEN)
+  const drawerModes = { OPEN: 'OPEN', CLOSED: 'CLOSED' }
+  const drawer = {
+    mode: drawerModes.OPEN,
+    buttonSymbol: buttonSymbols.OPEN,
+  }
+  const [state, setState] = useState(drawer)
+  useEffect(() => {
+    const mode = isOpen ? drawerModes.OPEN : drawerModes.CLOSED
+    const buttonSymbol = isOpen ? buttonSymbols.CLOSED : buttonSymbols.OPEN
+    setState(stateCurr => Object.assign({}, stateCurr, { mode, buttonSymbol }))
+    setXPos(xPositions[mode])
+  }, [isOpen])
   const styleDefinition = { top: yPos, [alignX]: xPos }
 
   return (
@@ -25,20 +38,20 @@ const DrawerContainer = ({ children, yPos = 20, alignX = DrawerAlign.LEFT }) => 
         style={{ [buttonPostion]: -50 }}
         type="button"
         onClick={() => {
-          const newX = xPos === openX ? closeX : openX
-          setXPos(newX)
+          setIsOpen(isOpenState => !isOpenState)
         }}
       >
-        {xPos === openX ? 'Close' : 'Open'}
+        {state.buttonSymbol}
       </button>
     </section>
   )
 }
 /* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/require-default-props */
 DrawerContainer.propTypes = {
   children: PropTypes.node.isRequired,
   yPos: PropTypes.number.isRequired,
-  alignX: PropTypes.string.isRequired,
+  alignX: PropTypes.string,
 }
 
 export default DrawerContainer
