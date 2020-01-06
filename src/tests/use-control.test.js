38cef926b0be16d64628652d59ebf40ad86fd241
Jose Collas <joseherminiocollas@goatstonc.com>
2020-01-06 07:34:25 -0800

    update test to use Enzyme

--- src/tests/use-control.test.js
+++ src/tests/use-control.test.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import useMapControl from '../hooks/use-map-control'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 const initLatLng = [0, 0]
 const expectedLatLng = [20, 20]
@@ -13,21 +17,41 @@ function TC() {
 
   return (
     <div>
-      DIVHOOK
-      <i>abc</i>
-      <div id="movecenter">{mapControl.moveCenterTo.toString()}</div>
-      <button
-        type="button"
-        onClick={() => actions.setMoveCenterTo(expectedLatLng)}
-      />
+      <div id="move-center-by">
+        <i>{mapControl.moveCenterTo.toString()}</i>
+        <button
+          type="button"
+          onClick={() => actions.setMoveCenterBy(expectedLatLng)}
+        />
+      </div>
+      <div id="move-center-to">
+        <i>{mapControl.moveCenterTo.toString()}</i>
+        <button
+          type="button"
+          onClick={() => actions.setMoveCenterTo(expectedLatLng)}
+        />
+      </div>
+      <div id="move-marker-by">
+        <i>{mapControl.moveMarkerTo.toString()}</i>
+        <button
+          type="button"
+          onClick={() => actions.setMoveMarkerBy(expectedLatLng)}
+        />
+      </div>
     </div>
   )
 }
-test('useControl', async () => {
-  const { getByText } = render(<TC />)
-  const el = getByText('DIVHOOK')
-  expect(el.querySelector('i').innerHTML).toBe('abc')
-  el.querySelector('button').click()
-  expect(el.querySelector('#movecenter').innerHTML)
-    .toBe(expectedLatLng.toString())
+describe('useControl', () => {
+  test('should have certain elements', async () => {
+    let wrapper
+    act(() => {
+      wrapper = mount(<TC />)
+    })
+    expect(wrapper.find('#move-center-by').length).toBe(1)
+    expect(wrapper.find('#move-center-to').length).toBe(1)
+    expect(wrapper.find('#move-marker-by').length).toBe(1)
+    expect(wrapper.find('#move-marker-by button').length).toBe(1)
+    expect(wrapper.find('#move-marker-by i').length).toBe(1)
+  })
+
 })
