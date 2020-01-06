39a509d298ad3d7f1378434ef52222f261eec5d8
Jose Collas <joseherminiocollas@goatstonc.com>
2020-01-06 08:40:16 -0800

    remove unused files

--- src/tests/use-center.test.js
+++
@@ -1,7 +0,0 @@
-import useCenter from '../use-center'
-
-describe('use-center', () => {
-  it('should be a function', () => {
-    expect(typeof useCenter).toBe('function')
-  })
-})
--- src/tests/use-map.test.js
+++
@@ -1,7 +0,0 @@
-import useMap from '../use-map'
-
-describe('use-map', () => {
-  it('should be a function', () => {
-    expect(typeof useMap).toBe('function')
-  })
-})
