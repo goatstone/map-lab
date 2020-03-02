181a744802172acbddb51a10a2d05992fe638fa8
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2020-03-02 23:46:15 +0000

    refactor GoToPlace component

--- src/components/GoToPlace.js
+++ src/components/GoToPlace.js
@@ -1,37 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+function GoToPlaceButton({ latLn, label, setMoveCenterBy }) {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        setMoveCenterBy(latLn)
+      }}
+    >
+      {label}
+    </button>
+  )
+}
+/* eslint-disable react/forbid-prop-types */
+GoToPlaceButton.propTypes = {
+  latLn: PropTypes.array.isRequired,
+  label: PropTypes.string.isRequired,
+  setMoveCenterBy: PropTypes.func.isRequired,
+}
+
+const buttons = [
+  ['Seattle', [47.6, -122.3]],
+  ['New York', [40.7128, -74.0060]],
+  ['Los Angeles', [34.0522, -118.2437]],
+]
+export { buttons }
+
 const GoToPlace = ({
   setMoveCenterBy,
 }) => (
   <section data-id="goto-place">
-    <button
-      type="button"
-      onClick={() => {
-        setMoveCenterBy([47.6, -122.3])
-      }}
-    >
-      Seattle
-    </button>
-    <button
-      type="button"
-      onClick={() => {
-        setMoveCenterBy([40.7128, -74.0060])
-      }}
-    >
-      New York
-    </button>
-    <button
-      type="button"
-      onClick={() => setMoveCenterBy([34.0522, -118.2437])}
-    >
-      LA
-    </button>
+    {buttons.map(b => (
+      <GoToPlaceButton
+        latLn={[47.6, -122.3]}
+        label="Seattle"
+        setMoveCenterBy={setMoveCenterBy}
+        key={b[0]}
+      />
+    ))}
   </section>
 )
 
-/* eslint-disable react/forbid-prop-types */
-/* eslint-disable react/require-default-props */
 GoToPlace.propTypes = {
   setMoveCenterBy: PropTypes.func.isRequired,
 }
