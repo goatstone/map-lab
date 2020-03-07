d1c8192321ce7ac5a8d13edd1d378bd801f2f4f2
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2020-03-07 04:22:21 +0000

    add test for StateDebug

---
+++ src/tests/StateDebug.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { act } from 'react-dom/test-utils'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import StateDebug from '../components/StateDebug'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<StateDebug />', () => {
+  it('should mount', () => {
+    let wrapper
+    act(() => {
+      wrapper = shallow(
+        <StateDebug
+          mapStatus={{ a: 1 }}
+          isShow={false}
+        />,
+      )
+    })
+    expect(wrapper).toBeTruthy()
+  })
+})
