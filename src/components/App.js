e29d8f0ce552b607a209cc719f06de87bb9feccc
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2020-05-11 10:35:13 -0700

    add Fluent Command Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,68 @@
 import React, { useReducer, useEffect } from 'react'
+import { CommandBar, initializeIcons } from '@fluentui/react'
 import jss from 'jss'
 import preset from 'jss-preset-default'
 import style from '../style/main-style'
 import GMap from './GMap'
 import LMap from './Map'
 
+initializeIcons()
+
 jss.setup(preset())
 const sheet = jss.createStyleSheet(style)
 sheet.attach()
+
+const items = [
+  {
+    key: 'newItem',
+    text: 'A',
+    cacheKey: 'a', // changing this key will invalidate this item's cache
+    iconProps: { iconName: 'Add' },
+    subMenuProps: {
+      items: [
+        {
+          key: 'b',
+          text: 'B',
+          iconProps: { iconName: 'Mail' },
+          onClick: () => console.log('b'),
+        },
+        {
+          key: 'c',
+          text: 'C',
+          iconProps: { iconName: 'Calendar' },
+        },
+      ],
+    },
+  },
+  {
+    key: 'd',
+    text: 'D',
+    iconProps: { iconName: 'Share' },
+    onClick: () => console.log('d'),
+  },
+]
+
+const overflowItems = [
+  {
+    key: 'move',
+    text: 'Z',
+    onClick: () => console.log('z'),
+    iconProps: { iconName: 'MoveToFolder' },
+  },
+]
+
+const farItems = [
+  {
+    key: 'info',
+    text: 'Info',
+    // This needs an ariaLabel since it's icon-only
+    ariaLabel: 'Info',
+    iconOnly: true,
+    iconProps: { iconName: 'Info' },
+    onClick: () => console.log('Info'),
+  },
+]
+const overflowProps = { ariaLabel: 'More commands' }
 
 function App() {
   const initState = { center: [47.6, -122.3], callerId: null }
@@ -40,32 +95,41 @@ function App() {
   }, [status])
 
   return (
-    <section className={sheet.classes.mainContainer}>
-      <div className={sheet.classes.frame}>
-        <GMap
-          mainClassName={sheet.classes.gMap}
-          control={control}
-          statusDispatch={statusDispatch}
+    <>
+      <section className={sheet.classes.mainContainer}>
+        <CommandBar
+          items={items}
+          overflowItems={overflowItems}
+          overflowButtonProps={overflowProps}
+          farItems={farItems}
+          ariaLabel="Use left and right arrow keys to navigate between commands"
         />
-        <LMap
-          mainClassName={sheet.classes.lMap}
-          control={control}
-          statusDispatch={statusDispatch}
-          idName="leaflet"
-        />
-      </div>
-      <footer>
-        <span>
-          Navigate A Map With A Map
-        </span>
-        <a href="https://www.goatstone.com" target="new">
-          Goatstone
-        </a>
-        <a href="https://github.com/JoseHerminioCollas/map-lab" target="new">
-          More Information
-        </a>
-      </footer>
-    </section>
+        <div className={sheet.classes.frame}>
+          <GMap
+            mainClassName={sheet.classes.gMap}
+            control={control}
+            statusDispatch={statusDispatch}
+          />
+          <LMap
+            mainClassName={sheet.classes.lMap}
+            control={control}
+            statusDispatch={statusDispatch}
+            idName="leaflet"
+          />
+        </div>
+        <footer>
+          <span>
+            Navigate A Map With A Map
+          </span>
+          <a href="https://www.goatstone.com" target="new">
+            Goatstone
+          </a>
+          <a href="https://github.com/JoseHerminioCollas/map-lab" target="new">
+            More Information
+          </a>
+        </footer>
+      </section>
+    </>
   )
 }
 
