155768aff9e4a06ac1f5905dbc7bb212d5c6165f
joseherminiocollas <joseherminiocollas@goatstone.com>
2021-01-06 13:55:22 -0800

    update AppService API with center functionality and test

--- src/app-service.test.ts
+++ src/app-service.test.ts
@@ -1,5 +1,6 @@
 import ApplicationService, { AppServiceInstanceI } from './app-service'
 
+const id = 0
 describe('ApplicationService', () => {
   it('should be truthy', () => {
     const appService: AppServiceInstanceI = ApplicationService()
@@ -11,7 +12,16 @@ describe('ApplicationService', () => {
     appService.addMessageEventListener(message => {
       expect(message).toBe(expectedMessage)
       done()
-    }, 0)
+    }, id)
     appService.addMessage(expectedMessage, 0)
+  })
+  it('should provide center values when they are added', done => {
+    const expectedCenter: number[] = [0, 0]
+    const appService: AppServiceInstanceI = ApplicationService()
+    appService.addCenterEventListener(centerValue => {
+      expect(centerValue).toBe(expectedCenter)
+      done()
+    }, id)
+    appService.addCenterStatus(expectedCenter, 0)
   })
 })
--- src/app-service.ts
+++ src/app-service.ts
@@ -15,19 +15,35 @@ export interface AppServiceI {
   (): {
     addMessageEventListener: AddMessageEventListener;
     addMessage: AddMessage;
+    addCenterEventListener: AddCenterEventListener;
+    addCenterStatus: AddCenterStatus;
   }
 }
 export interface AppServiceInstanceI {
   addMessageEventListener: AddMessageEventListener;
   addMessage: AddMessage;
+  addCenterEventListener: AddCenterEventListener;
+  addCenterStatus: AddCenterStatus;
 }
 export interface MessageEventListener {
   listener: any;
   id: number;
 }
+interface AddCenterEventListener {
+  (listener: (center: number[])=> void, id: number): void
+}
+export interface Center {
+  center: number[]
+  id: number
+}
+interface AddCenterStatus {
+  (center: number[], id: number) : void
+}
 
 const AppService: AppServiceI = () => {
   const messages$: Subject<Message> = new Subject()
+  const centers$: Subject<Center> = new Subject()
+
   const mT = messages$.pipe(
     map(message => message),
   )
@@ -46,10 +62,25 @@ const AppService: AppServiceI = () => {
       }]
     mT.subscribe(...subArgs)
   }
-
+  const addCenterEventListener: AddCenterEventListener = (listener, id) => {
+    centers$.subscribe((center: Center) => {
+      listener(center.center)
+    },
+    // eslint-disable-next-line no-console
+    console.log,
+    () => {
+      // eslint-disable-next-line no-console
+      console.log('Completed: ', id)
+    })
+  }
+  const addCenterStatus: AddCenterStatus = (center, id) => {
+    centers$.next({ center, id })
+  }
   return {
     addMessageEventListener,
     addMessage,
+    addCenterEventListener,
+    addCenterStatus,
   }
 }
 
