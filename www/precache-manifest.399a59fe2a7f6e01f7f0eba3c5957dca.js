da9aa6b0d3844200e65b39de00077f3b2336899b
joseherminiocollas <joseherminiocollas@goatstone.com>
2021-01-14 13:52:45 -0800

    build for production

---
+++ www/precache-manifest.399a59fe2a7f6e01f7f0eba3c5957dca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9804b6f3a1c1f803c807b7483f4b0dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f51dc0318217523e25a",
+    "url": "/static/css/main.207e1da6.chunk.css"
+  },
+  {
+    "revision": "083a57bea30059d61b06",
+    "url": "/static/js/2.f6261b12.chunk.js"
+  },
+  {
+    "revision": "9f51dc0318217523e25a",
+    "url": "/static/js/main.17d11e9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
