d89f4c1202e95af2ee7152f8488274ef2bc92482
Jose Collas <joseherminiocollas@goatstone.com>
2021-01-24 18:53:35 -0800

    update for eslint failure fix

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,7 @@ const config = {
 try {
   // eslint-disable-next-line
   const gKey = require('./g-key').default
+  // eslint-disable-next-line
   const configUpdate = require('./bingmap-key').default
   Object.assign(config, gKey, configUpdate)
 } catch (e) {
